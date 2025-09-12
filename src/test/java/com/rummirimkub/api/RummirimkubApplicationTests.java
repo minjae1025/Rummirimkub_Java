97cf66f2ce7e5c45c18eb867d2844837ffdad95a
minjae1025 <kjt081025@gmail.com>
2025-09-12 23:21:31 +0900

    firebase 연동

---
+++ src/test/java/com/rummirimkub/api/RummirimkubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rummirimkub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RummirimkubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
